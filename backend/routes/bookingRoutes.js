f3ea42df12f30c76612d048ea220024fdb45596c
DPRCHANUKA <rukshanchanuka.2002@gmail.com>
2026-03-24 20:25:14 +0530

    modifield modules and and routes and improve bookingseat function

--- backend/routes/bookingRoutes.js
+++ backend/routes/bookingRoutes.js
@@ -2,21 +2,34 @@ const express = require("express");
 const router = express.Router();
 const Booking = require("../models/Booking");
 
-// POST booking
+// CREATE booking
 router.post("/", async (req, res) => {
   try {
-    const booking = new Booking(req.body);
-    await booking.save();
-    res.json({ message: "Booking saved", booking });
+    const { name, studentId, seat, date, fromTime, toTime } = req.body;
+
+    // prevent double booking
+    const existing = await Booking.findOne({ seat, date, fromTime, toTime });
+
+    if (existing) {
+      return res.status(400).json({ message: "Seat already booked!" });
+    }
+
+    const newBooking = new Booking({
+      name,
+      studentId,
+      seat,
+      date,
+      fromTime,
+      toTime
+    });
+
+    await newBooking.save();
+
+    res.json({ message: "Booking saved!" });
+
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
-});
-
-// GET bookings
-router.get("/", async (req, res) => {
-  const bookings = await Booking.find();
-  res.json(bookings);
 });
 
 module.exports = router;
